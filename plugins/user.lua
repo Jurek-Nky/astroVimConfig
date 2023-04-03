c633fe2a1dedd3af6f162492e13824a7fad0891f
Jurek Nitschky <q30@tuta.io>
2023-04-03 21:55:20 +0200

    new theme

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,6 +1,30 @@
-return {
+options = {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = require("user.plugins.options.neo-tree")
-  }
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      style = "night",    -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+      light_style = "day", -- The theme is used when the background is set to light
+      transparent = false, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark",          -- style for sidebars, see below
+        floats = "dark",            -- style for floating windows
+      },
+    }
+  },
 }
+
+return options
