a229177772f3d138852075dbb6dffbdc93ade365
Jurek Nitschky <q30@tuta.io>
2023-04-03 16:30:40 +0200

    turn off auto formatting
    add visual and insert keymap list

--- mappings.lua
+++ mappings.lua
@@ -21,6 +21,8 @@ return {
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
+  v = {},
+  i = {},
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
