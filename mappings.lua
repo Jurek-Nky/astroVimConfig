377c7a8a5abd75ae6327dacc5276f4520e52d3a9
Jurek Nitschky <q30@tuta.io>
2023-04-03 16:42:38 +0200

    add force quit keybind

--- mappings.lua
+++ mappings.lua
@@ -6,6 +6,7 @@
 return {
   -- first key is the mode
   n = {
+    ["q"] = {"<cmd>q!<cr>", desc = "force quit"},
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
