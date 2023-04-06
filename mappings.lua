7bd9e8d5e86d80e83a51c7a8d2b28aaa3d0362b7
Jurek Nitschky <q30@tuta.io>
2023-04-06 11:25:04 +0200

    refactor mappings file
    new keybinds:
      qq -> <cmd>q<cr>
      qf -> <cmd>q!<cr>

--- mappings.lua
+++ mappings.lua
@@ -1,31 +1,20 @@
--- Mapping data with "desc" stored directly by vim.keymap.set().
---
--- Please use this mappings table to set keyboard mapping since this is the
--- lower level configuration and more robust one. (which-key will
--- automatically pick-up stored data by this setting.)
-return {
-  -- first key is the mode
-  n = {
-    ["q"] = {"<cmd>q!<cr>", desc = "force quit"},
-    -- second key is the lefthand side of the map
-    -- mappings seen under group name "Buffer"
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<leader>bD"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
-      end,
-      desc = "Pick to close",
-    },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-  },
-  v = {},
-  i = {},
-  t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
-  },
+local utils = require "astronvim.utils"
+local maps = { n = {}, v = {}, i = {}, t = {} }
+-- ############################
+-- general 
+-- ############################
+-- rebinding quit key to force quit
+maps.n["qf"] = { "<cmd>q!<cr>", desc = "force quit" }
+maps.n["qq"] = { "<cmd>q<cr>", desc = "quit" }
+-- ############################
+-- buffers
+-- ############################
+maps.n["<leader>bD"] = {
+  function()
+    require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+      require("astronvim.utils.buffer").close(
+        bufnr)
+    end)
+  end,
+  desc = "Pick to close",
 }
